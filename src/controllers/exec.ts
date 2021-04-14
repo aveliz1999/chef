aef50998a09adebc1e3587bf61914f59753fe0d8
Veliz Murrieta <avelizmu@mdc.edu>
2021-04-14 08:01:01 -0400

    Remove debug messages

--- src/controllers/exec.ts
+++ src/controllers/exec.ts
@@ -15,7 +15,6 @@ const languages = {
 }
 
 export const exec = async function(req: Request, res: Response) {
-    console.time('fullRun');
     if(!Object.keys(languages).includes(req.body.language)) {
         return res.status(400).send({message: 'Unsupported language'});
     }
@@ -23,10 +22,8 @@ export const exec = async function(req: Request, res: Response) {
 
     const id = uuid();
 
-    console.time('fs');
     await fs.promises.mkdir(`${os.tmpdir()}/${id}`);
     await fs.promises.writeFile(`${os.tmpdir()}/${id}/exec`, req.body.code);
-    console.timeEnd('fs');
     let data = '';
 
     const stream = new Writable();
@@ -34,18 +31,12 @@ export const exec = async function(req: Request, res: Response) {
         data += chunk;
     }
 
-    console.time('pull');
     await docker.pull(language.image);
-    console.timeEnd('pull');
-    console.time('run');
     const [result,_] = await docker.run(language.image, language.command, stream, {
         HostConfig: {
             Binds: [`${os.tmpdir()}/${id}:/app`]
         }
     });
-    console.timeEnd('run');
-    console.timeEnd('fullRun');
-    console.log(`Result: ${data}`);
 
     if(result.StatusCode) {
         res.status(400);
