0c9fcc5268ad815948feb8b9f6b77fd777a83663
aveliz1999 <github@veliz99.com>
2021-05-29 18:43:33 -0400

    Add rate limit functionality

--- config/index.ts
+++ config/index.ts
@@ -20,6 +20,13 @@ export type DatabaseConfig = {
     dialect: Dialect
 }
 
+export type RateLimitConfig = {
+    timeInMs: number,
+    maxRequestsUnauthenticated: number,
+    maxRequestsAuthenticated: number
+}
+
 export const jwtConfig: JWTConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'jwt.yaml'), 'utf-8'));
 export const githubConfig: GithubConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'github.yaml'), 'utf-8'));
 export const databaseConfig: DatabaseConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'database.yaml'), 'utf-8'));
+export const rateLimitConfig: RateLimitConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'rateLimit.yaml'), 'utf-8'));
--- src/routes/exec.ts
+++ src/routes/exec.ts
@@ -2,14 +2,46 @@ import express from 'express';
 import {exec, versions} from '../controllers/exec';
 import jwt from 'express-jwt';
 import {jwtConfig} from '../../config';
+import {rateLimitConfig} from "../../config";
+import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
 
-router.post('/', jwt({
+router.get('/versions', versions);
+
+router.use(jwt({
     secret: jwtConfig.secret,
     algorithms: ['HS256'],
     credentialsRequired: false
-}), exec);
+}));
+
+// TODO switch from using MemoryStore to another storage. Redis recommended
+router.use(rateLimit({
+    windowMs: rateLimitConfig.timeInMs,
+    max: (req, res): number => {
+        if(req.user) {
+            return rateLimitConfig.maxRequestsAuthenticated
+        }
+        return rateLimitConfig.maxRequestsUnauthenticated;
+    },
+    keyGenerator: (req, res): string => {
+        // Rate limit by github ID if authenticated or by IP if not
+        if(req.user) {
+            return `GITHUB_${req.user.githubId}`;
+        }
+        return `IP_${req.ip}`
+    },
+    handler: (req, res) => {
+        if(!req.user) {
+            return res.status(429).send({message: 'Too many requests, please try again later. ' +
+                    `Authenticated to increase the rate limit from ${rateLimitConfig.maxRequestsUnauthenticated} to ` +
+                    `${rateLimitConfig.maxRequestsAuthenticated}.`});
+        }
+        return res.status(429).send({message: 'Too many requests, please try again later.'});
+    }
+}))
+
+router.post('/', exec);
 
 router.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
@@ -19,7 +51,5 @@ router.use(function (err, req, res, next) {
         next();
     }
 });
-
-router.get('/versions', versions);
 
 export default router;
